0a5dc606b500d956c16c3b1b15a2e6cb819c22a9
chldydgh4687 <s2blue4687@naver.com>
2019-06-06 05:18:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import cv2
 
 #image capture class
-
+print("1")
 vidcap = cv2.VideoCapture('/test_data/demo.mp4')
 
 count = 0
@@ -10,9 +10,9 @@ count = 0
 while(vidcap.isOpened()):
 
     ret, image = vidcap.read()
-
+    print("2")
     cv2.imwrite("/images/frame%d.jpg"% count, image)
-
+    print("3")
     print('Saved frame%d.jpg' % count)
     count += 1
 
