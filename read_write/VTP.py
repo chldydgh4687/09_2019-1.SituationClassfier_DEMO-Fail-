969ef14484966a232f2b097af1a8ccb1f457c052
chldydgh4687 <s2blue4687@naver.com>
2019-06-06 09:54:04 +0900

    VTP
    not video_to_picture but VTP.

--- read_write/VTP.py
+++ read_write/VTP.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
-
 import argparse
 import cv2
 
-vidcap = cv2.VideoCapture('./test_data/demo.mp4')
-success,image = vidcap.read()
-count = 0;
-while success:
+def video_to_picture(op,wp):
+    vidcap = cv2.VideoCapture(op)
     success,image = vidcap.read()
-    cv2.imwrite("./images/frame%d.jpg"% count, image)     # save frame as JPEG file
-    if cv2.waitKey(50) == 27:                     # exit if Escape is hit
-        break
-    count += 1
+    count = 0;
+    while success:
+        success,image = vidcap.read()
+        cv2.imwrite(wp+"/frame%d.jpg"% count, image)
+        print("frame%d.jpg extract success"% count)
+        # save frame as JPEG file
+        if cv2.waitKey(50) == 27:
+            # exit if Escape is hit
+            break
+        count += 1
 
 
 #when we are possible real time, we must find new code.. 
