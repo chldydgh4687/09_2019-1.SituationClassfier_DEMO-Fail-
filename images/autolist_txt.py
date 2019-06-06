87312bee3d449eec52e3200b65396a4f1366969c
chldydgh4687 <s2blue4687@naver.com>
2019-06-07 05:08:07 +0900

    Update autolist_txt.py

--- images/autolist_txt.py
+++ images/autolist_txt.py
@@ -7,8 +7,8 @@ def autolist():
     f = open(os.getcwd()+'/images/data_list.txt','w')
     folder=basename('/SituationClassfier_DEMO/images')
     print(basename('./SituationClassfier_DEMO/images'))
-    print("1")
     for i in glob.iglob('*.jpg'):
+        print("1")
         data = folder + "/" + i + "\n"
         f.write(data)
     f.close()
