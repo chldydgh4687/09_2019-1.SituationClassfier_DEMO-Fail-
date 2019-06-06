254088d4fca01fea4ad6ae1ab23f3ca00d79a74c
chldydgh4687 <s2blue4687@naver.com>
2019-06-06 17:31:22 +0900

    Update test_davis_videos.py

--- Depth_e/test_davis_videos.py
+++ Depth_e/test_davis_videos.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import torch
-import lib.train_options 
+import Depth_e.options import train_options 
 from loaders import aligned_data_loader
 from models import pix2pix_model
 
