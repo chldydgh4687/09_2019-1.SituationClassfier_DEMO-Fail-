9647e5bf718129fd6caac50120eaef02449612c7
chldydgh4687 <s2blue4687@naver.com>
2019-06-06 09:24:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 # -*- coding: utf-8 -*-
+import argparse
 import cv2
 
-#image capture class
-print("1")
 vidcap = cv2.VideoCapture('./test_data/demo.mp4')
-print("video")
-count = 0
-
-while(vidcap.isOpened()):
- 
-    ret, image = vidcap.read()
-    cv2.imwrite("./images/frame%d.jpg"% count, image)
-    print("3")
-    print('Saved frame%d.jpg' % count)
+success,image = vidcap.read()
+count = 0;
+while success:
+    success,image = vidcap.read()
+    cv2.imwrite("./images/frame%d.jpg"% count, image)     # save frame as JPEG file
+    if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+        break
     count += 1
-    print(count)
-vidcap.release()
