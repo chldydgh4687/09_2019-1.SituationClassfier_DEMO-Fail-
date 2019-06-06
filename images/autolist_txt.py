bb85741bb5f109410fdb8100d519f2cc5cd4fd3f
chldydgh4687 <s2blue4687@naver.com>
2019-06-07 05:04:33 +0900

    Update autolist_txt.py

--- images/autolist_txt.py
+++ images/autolist_txt.py
@@ -3,14 +3,13 @@ import glob
 import os
 
 def autolist():
-    print(os.getcwd()+'data_list.txt')
+    
     f = open(os.getcwd()+'/data_list.txt','w')
-    print("1")
-    folder=basename('./SituationClassfier_DEMO/images')
+    folder=basename('/SituationClassfier_DEMO/images')
     print(basename('./SituationClassfier_DEMO/images'))
     print("1")
     for i in glob.iglob('*.jpg'):
-        data = a+"/"+i+"\n"
+        data = folder + "/" + i + "\n"
         f.write(data)
     f.close()
 
