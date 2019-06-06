bb745aa96af9b182519ec1dc0a6a5a710ee60700
chldydgh4687 <s2blue4687@naver.com>
2019-06-06 09:28:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ count = 0;
 while success:
     success,image = vidcap.read()
     cv2.imwrite("./images/frame%d.jpg"% count, image)     # save frame as JPEG file
-    if cv2.waitKey(10) == 27:                     # exit if Escape is hit
+    if cv2.waitKey(20) == 27:                     # exit if Escape is hit
         break
     count += 1
