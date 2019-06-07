6e95d0ac8e14f048c0523285fdcc6f13893de255
chldydgh4687 <s2blue4687@naver.com>
2019-06-07 19:49:33 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -11,4 +11,4 @@ def detection():
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images")
     print(os.getcwd())
-    rgbimg=cv2.imread('frame00000.jpg')
+    rgbimg=cv2.imread('images/frame00000.jpg')
