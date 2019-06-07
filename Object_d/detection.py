88ca4acd39b9691aabf56eb1ebd40944d75ad41c
chldydgh4687 <s2blue4687@naver.com>
2019-06-07 20:04:28 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -2,6 +2,7 @@
 import os
 import numpy as np
 import cv2
+from IPython.display import Image
 
 # load a image
 
@@ -12,7 +13,11 @@ def detection():
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
     rgbimg=cv2.imread('frame00000.jpg')
-    cv2.imshow('original',rgbimg)
-
+    Image(rgbimg)
+    
+  ##  cv2.imshow('original',rgbimg)
+  ##colab have not x-server
+    
+    
     cv2.waitkey(0)
     cv2.destroyAllWindows()
