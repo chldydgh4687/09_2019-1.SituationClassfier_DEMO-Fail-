e6a774d03c32502fa18679a9ea877684af6d61b1
chldydgh4687 <s2blue4687@naver.com>
2019-06-07 19:42:10 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -10,4 +10,5 @@ def detection():
 ## SSD
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))]))
+    print(os.getcwd())
     rgbimg=cv2.imread('images/frame00000.jpg')
