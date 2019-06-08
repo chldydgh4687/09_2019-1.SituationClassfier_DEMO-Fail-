1856014434e7a0e6cbf6e5ea1c128f9799ed192f
chldydgh4687 <s2blue4687@naver.com>
2019-06-08 13:59:12 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -1,7 +1,7 @@
 #Fixxing gt
 import os
 import numpy as np
-from google.colab.patches import cv2_imshow
+import matplotlib.pyplot as plt
 import cv2
 
 
@@ -13,11 +13,12 @@ def detection():
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
-    
-    rgbimg=cv2.imread('frame00000.jpg',cv2.IMREAD_UNCHANGED)
-    resized = cv2.resize(rgbimg,(256,256))
-    cv2_imshow(resized)
-    
+    img = cv2.imread('frame00000.jpg')
+    print(img)
+    plt.figure(figsize=(18,18))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
   ##  cv2.imshow('original',rgbimg)
   ##colab have not x-server, so ipython used
     
