1084b2dfb980531d0e4a8bd8ba317323f445ca41
chldydgh4687 <s2blue4687@naver.com>
2019-06-08 13:19:38 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -1,8 +1,9 @@
 #Fixxing gt
 import os
 import numpy as np
+from google.colab.patches import cv2_imshow
 import cv2
-from IPython.display import Image
+
 
 # load a image
 
@@ -12,8 +13,9 @@ def detection():
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
+    
     rgbimg=cv2.imread('frame00000.jpg')
-    Image('frame00000.jpg')
+    cv2_imshow(rgbimg)
     
   ##  cv2.imshow('original',rgbimg)
   ##colab have not x-server, so ipython used
