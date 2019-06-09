a78ae22d739c290c016095ce984fbbd61885e7c8
chldydgh4687 <s2blue4687@naver.com>
2019-06-09 13:14:53 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -12,8 +12,9 @@ def detection():
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
-    rgbimg=cv2.imread('frame00000.jpg')
-    Image('frame00000.jpg')
+    img = cv2.imread('frame00000.jpg')
+    img2 = img(:,:,::-1)
+    plt.imshow(img)
     
   ##  cv2.imshow('original',rgbimg)
   ##colab have not x-server, so ipython used
