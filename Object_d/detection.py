b941cbe2652e67665f9f1b8a49860d64d4520850
chldydgh4687 <s2blue4687@naver.com>
2019-06-13 17:22:35 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -12,15 +12,17 @@ def detection():
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
-    img = cv2.imread('frame00000.jpg')
-    cv2.rectangle(img,(250,0),(350,300), (255,0,0), 3)
-    cv2.imwrite('t_frame00000.jpg',img)
-
-    os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
-    print(os.getcwd())
     img = cv2.imread('frame00020.jpg')
-    cv2.rectangle(img,(270,0),(350,320), (255,0,0), 3)
+    cv2.rectangle(img,(220,0),(320,300), (255,0,0), 3)
     cv2.imwrite('t_frame00020.jpg',img)
-  ##  cv2.imshow('original',rgbimg)
+    
+    img = cv2.imread('frame00040.jpg')
+    cv2.rectangle(img,(150,0),(300,400), (255,0,0), 3)
+    cv2.imwrite('t_frame00040.jpg',img)
+    
+    img = cv2.imread('frame00060.jpg')
+    cv2.rectangle(img,(10,0),(280,480), (255,0,0), 3)
+    cv2.imwrite('t_frame00060.jpg',img)
+  ##cv2.imshow('original',rgbimg)
   ##colab have not x-server, so ipython used
     
