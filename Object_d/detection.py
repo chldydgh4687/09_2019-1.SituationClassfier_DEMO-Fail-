c29aeac3727c2b3029cffa2a89fe8a4b3bf3056f
chldydgh4687 <s2blue4687@naver.com>
2019-06-13 10:07:13 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -13,7 +13,7 @@ def detection():
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
     img = cv2.imread('frame00000.jpg')
-    cv2.rectangle(img,(250,0),(300,400), (255,0,0), 3)
+    cv2.rectangle(img,(250,0),(350,300), (255,0,0), 3)
     cv2.imwrite('t_frame00000.jpg',img)
     
   ##  cv2.imshow('original',rgbimg)
