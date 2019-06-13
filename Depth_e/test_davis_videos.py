b69eb087a3e44a415d20539972492db896372d24
chldydgh4687 <s2blue4687@naver.com>
2019-06-13 22:02:24 +0900

    Update test_davis_videos.py

--- Depth_e/test_davis_videos.py
+++ Depth_e/test_davis_videos.py
@@ -52,5 +52,5 @@ def td_Depth():
         
 def D_mpointbox(rect, rect2):
     print(os.getcwd())
-    os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/viz_predictions")
+    os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"images/viz_predictions")
     print(os.getcwd())
