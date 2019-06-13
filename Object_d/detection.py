60b10c77a6b830cd8d17ad94e9d278bd0d567c47
chldydgh4687 <s2blue4687@naver.com>
2019-06-13 21:20:58 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -37,6 +37,37 @@ return xy, xy1, xy2
 
 def movetoDep(p1,p2,p3):
 
-    
+    # p = [x,y,w,h]
+  if(p1[0]>p2[0]+p2[2]):
+    return null
+  if(p1[0]+p1[2]<p2[0]):
+    return null
+  if(p1[1]>p2[1]+p2[3]):
+    return null
+  if(p1[1]+p1[3]<p2[1]):
+    return null
+  
+  rect = [0,0,0,0]
+  rect[0] = max(p1[0],p2[0])
+  rect[1] = max(p1[1],p2[1])
+  rect[2] = min(p1[0]+p1[2],p2[0]+p2[2])-rect[0]
+  rect[3] = min(p1[1]+p1[3],p2[1]+p2[3])-rect[1]
+  
+  if(p2[0]>p3[0]+p3[2]):
+    return null
+  if(p2[0]+p2[2]<p3[0]):
+    return null
+  if(p2[1]>p3[1]+p3[3]):
+    return null
+  if(p2[1]+p2[3]<p3[1]):
+    return null
+  
+  rect2 = [0,0,0,0]
+  rect2[0] = max(p2[0],p3[0])
+  rect2[1] = max(p2[1],p3[1])
+  rect2[2] = min(p2[0]+p2[2],p3[0]+p3[2])-rect2[0]
+  rect2[3] = min(p2[1]+p2[3],p3[1]+p3[3])-rect2[1]
+  
+  return rect, rect2
 
-    
+### 일단 수동적임을 말씀드립니다...추후 재귀로 짤생각..
