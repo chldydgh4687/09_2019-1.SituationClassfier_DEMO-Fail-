a9fb274e68b1822f741d0e68924fc4aec73a764e
chldydgh4687 <s2blue4687@naver.com>
2019-06-13 18:23:14 +0900

    Update detection.py

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -8,21 +8,25 @@ from matplotlib import pyplot as plt
 
 def detection():
 
+
 ## SSD
     
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"/images/")
     print(os.getcwd())
     img = cv2.imread('frame00020.jpg')
     cv2.rectangle(img,(220,0),(320,300), (255,0,0), 3)
-    cv2.imwrite('t_frame00020.jpg',img)
+    xy =([220,0,320,300]) ##extraction x1y1,x2y2 dot 
+    cv2.imwrite('t_frame00020.jpg',img)  ## Saving detected picture 
     
     img = cv2.imread('frame00040.jpg')
     cv2.rectangle(img,(150,0),(300,400), (255,0,0), 3)
-    cv2.imwrite('t_frame00040.jpg',img)
+    xy1 =([150,0,300,400]) ##extraction x1y1,x2y2 dot  
+    cv2.imwrite('t_frame00040.jpg',img)  ## Saving detected picture 
     
     img = cv2.imread('frame00060.jpg')
     cv2.rectangle(img,(10,0),(280,480), (255,0,0), 3)
-    cv2.imwrite('t_frame00060.jpg',img)
-  ##cv2.imshow('original',rgbimg)
-  ##colab have not x-server, so ipython used
+    xy2 =([150,0,300,400])  ##extraction x1y1,x2y2 dot 
+    cv2.imwrite('t_frame00060.jpg',img) ## Saving detected picture 
+
     
+return xy, xy1, xy2
