125598883983aeb4a796bab2f548152e5e9a196b
chldydgh4687 <s2blue4687@naver.com>
2019-06-14 11:27:48 +0900

    fdg
    dgfdgd

--- Object_d/detection.py
+++ Object_d/detection.py
@@ -43,21 +43,37 @@ def de_box(p1,p2,p3):
     p2_d = [(int)(0.8*(p2[0]+((p2[2]-p2[0])/2))),(int)(0.6*(p2[1]+((p2[3]-p2[1])/2)))]
     p3_d = [(int)(0.8*(p3[0]+((p3[2]-p3[0])/2))),(int)(0.6*(p3[1]+((p3[3]-p3[1])/2)))]
 
+    P_list = [p1_d,p2_d,p3_d]
     ## 사진이 512x 288 로 변경됨. 비율 계산식이 필요 
     ## 세로 비율 0.6, 가로 비율 0.8
+
     os.chdir(os.path.commonprefix([os.getcwd(),os.path.dirname(os.path.realpath(__file__))])+"Depth_e/viz_predictions/images")
     # p1_d = [x.y] to rectangle 
+    
+    mse_prevent(P_list)
+
+def mse_prevent(P_list):
+
     img = cv2.imread('frame00020.jpg')
-    cv2.circle(img,(p1_d[0],p1_d[1]),3,(255,0,0),-1)
-    cv2.rectangle(img,(p1_d[0]-25,p1_d[1]-25),(p1_d[0]+25,p1_d[1]+25),(255,0,0),3)
-    cv2.imwrite('m_frame00020.jpg',img)
 
-    img = cv2.imread('frame00040.jpg')
-    cv2.circle(img,(p2_d[0],p2_d[1]),3,(255,0,0),-1)
-    cv2.imwrite('m_frame00040.jpg',img)
 
-    img = cv2.imread('frame00060.jpg')
-    cv2.circle(img,(p3_d[0],p3_d[1]),3,(255,0,0),-1)
-    cv2.imwrite('m_frame00060.jpg',img)
+
+
+
+    ##guide_line
+
+    #img = cv2.imread('frame00020.jpg')
+    #cv2.circle(img,(p1_d[0],p1_d[1]),3,(255,0,0),-1)
+    #cv2.rectangle(img,(p1_d[0]-25,p1_d[1]-25),(p1_d[0]+25,p1_d[1]+25),(255,0,0),3)
+    #cv2.imwrite('m_frame00020.jpg',img)
+
+    #img = cv2.imread('frame00040.jpg')
+    #cv2.circle(img,(p2_d[0],p2_d[1]),3,(255,0,0),-1)
+    #cv2.imwrite('m_frame00040.jpg',img)
+
+    #img = cv2.imread('frame00060.jpg')
+    #cv2.circle(img,(p3_d[0],p3_d[1]),3,(255,0,0),-1)
+    #cv2.imwrite('m_frame00060.jpg',img)
+
 
 ### 일단 수동적임을 말씀드립니다...추후 recursive 로 짤생각..
