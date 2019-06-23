2ef40c0e7f0ae0790ed8188727254dcd7d8f1089
chldydgh4687 <s2blue4687@naver.com>
2019-06-23 23:45:22 +0900

    Update VTP.py

--- read_write/VTP.py
+++ read_write/VTP.py
@@ -8,14 +8,14 @@ def video_to_picture(op,wp):
     count = 0;
     while success:
         success,image = vidcap.read()
-        if count % 1== 0:
+        if count % 0.1== 0:
             cv2.imwrite(wp+"/frame%05d.jpg"% count, image)
             print("frame%05d.jpg extract success"% count)
         # save frame as JPEG file
         if cv2.waitKey(10) == 27:
             # exit if Escape is hit
             break
-        count += 1
+        count +=0.1
 
 
 #when we are possible real time, we must find new code.. 
